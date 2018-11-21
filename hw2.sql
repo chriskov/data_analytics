181b016499b40de525ea3393e6e68d1f6ff7791b
chriskov <chriskov@mail.ru>
2018-11-21 14:21:05 +0300

    Исправлен запрос 4.2

--- hw2.sql
+++ hw2.sql
@@ -1,3 +1,4 @@
+
 SELECT 'ФИО:  Чернова Кристина Сергеевна';
 -- 1.2
 SELECT * FROM links WHERE imdbid LIKE '%42' AND movieid BETWEEN 100 AND 1000 LIMIT 10;
@@ -11,5 +12,6 @@ SELECT userid, avg(rating) AS average_rating FROM ratings GROUP BY userid having
 SELECT l1.movieid FROM links AS l1 INNER JOIN 
 (SELECT movieid, AVG(rating) FROM ratings GROUP BY movieid HAVING AVG(rating) > 3.5) AS r1 ON l1.movieid = r1.movieid LIMIT 10;
 -- 4.2
-SELECT AVG(rating) FROM ratings WHERE userid IN 
-(SELECT userid FROM (SELECT userid,count(*) as co from ratings GROUP BY userid) as test WHERE co>10);
+SELECT AVG(rating) FROM ratings INNER JOIN
+(SELECT userid as uid FROM ratings GROUP BY userid HAVING count(userid) > 10) AS rs	ON ratings.userid = rs.uid ;
+	
